57ced4eb435be7938789a5e31da203436e46916f
kanak826 <kanakbaghel64@gmail.com>
2025-05-01 10:49:03 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,45 @@
+#fibonacci series = [fn = (fn-1 + fn-2) ]
+
+# def fibonacci(n):
+    # if n==0:
+        # return 0
+    # return fibonacci
+# n=int(input("enter num :"))#memorization of top approach
+# n2=[-1]*(n+1)
+# def fb(n,n2):
+    # if n==0:
+        # return 0
+    # elif n==1: 
+        # return 1
+    # elif n2[n]!=-1:
+        # return n2[n]
+    # n2[n]=(fb(n-1,n2)+fb(n-2,n2))
+    # return n2[n]
+# print(fb,(n2,n))
+
+
+# n=int(input("enter num :"))#bottom up approach
+# dp=[0]*(n+1)
+# dp[0]=0
+# dp[1]=1
+# for i in range(2,n+1):
+    # dp[i]=dp[i-1]+dp[i-2]
+# print(dp)
+# print(dp[n])    
+
+
+#***********************************************************************
+# 
+
+
+
+
+
+
+
+
+
+
+
+
+
